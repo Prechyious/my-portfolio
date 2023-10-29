49b69536a16ce488baf9cdfd481058605111a48a
Prechyious <lemoprechy@gmail.com>
2023-10-29 07:31:44 +0100

    added project

--- src/component/data/Projects.js
+++ src/component/data/Projects.js
@@ -5,6 +5,7 @@ import artGallery from "../../assets/images/Art-gallery.png";
 import authenticate from "../../assets/images/Authenticate-me.png";
 import countries from "../../assets/images/countries.png";
 import battleship from "../../assets/images/battleship.png";
+import multiStepForm from "../../assets/images/mobile.png";
 
 export const projects = [
     {
@@ -38,32 +39,32 @@ export const projects = [
     },
     {
         id: 2,
-        img: portfolio,
-        title: "Portfolio Site",
+        img: multiStepForm,
+        title: "Multi Step Form",
         description:
-            "One of my first projects, a fully responsive portfolio website with theme toggle.",
+            "A multi-step form in sequence, you can go back to edit previous selections and then finally get a summary of all selections",
         stack: [
             {
-                name: "HTML",
-                bg: "bg-orange-400",
+                name: "React",
+                bg: "bg-sky-500",
             },
             {
-                name: "CSS",
-                bg: "bg-blue-600",
+                name: "Redux Toolkit",
+                bg: "bg-purple-800",
             },
             {
-                name: "JavaScript",
-                bg: "bg-amber-400",
+                name: "Tailwind",
+                bg: "bg-blue-800",
             },
         ],
         links: [
             {
                 demo: {
-                    link: "https://numo-stanley.netlify.app",
+                    link: "https://my-multi-step-form.vercel.app",
                     icon: <FaGlobe size={18} />,
                 },
                 github: {
-                    link: "https://github.com/Numostanley/MyPortfolio",
+                    link: "https://github.com/Prechyious/multi-step-form",
                     icon: <FaCode size={18} />,
                 },
             },
@@ -71,35 +72,6 @@ export const projects = [
     },
     {
         id: 3,
-        img: artGallery,
-        title: "Harvard Art Gallery",
-        description:
-            "A React frontend application that displays a list of art galleries. The information about the art galleries were extracted from the Harvard Art Museum API",
-        stack: [
-            {
-                name: "React",
-                bg: "bg-sky-500",
-            },
-            {
-                name: "CSS",
-                bg: "bg-blue-400",
-            },
-        ],
-        links: [
-            {
-                demo: {
-                    link: "https://youtu.be/E6z3twyKtDE",
-                    icon: <FaGlobe size={18} />,
-                },
-                github: {
-                    link: "https://github.com/Prechyious/long-practice-art-museum-project",
-                    icon: <FaCode size={18} />,
-                },
-            },
-        ],
-    },
-    {
-        id: 4,
         img: authenticate,
         title: "Authenticate",
         description: "An authentication app with Express and React",
@@ -143,7 +115,7 @@ export const projects = [
         ],
     },
     {
-        id: 5,
+        id: 4,
         img: countries,
         title: "Rest Countries",
         description:
@@ -172,7 +144,69 @@ export const projects = [
         ],
     },
     {
+        id: 5,
+        img: portfolio,
+        title: "Portfolio Site",
+        description:
+            "One of my first projects, a fully responsive portfolio website with theme toggle.",
+        stack: [
+            {
+                name: "HTML",
+                bg: "bg-orange-400",
+            },
+            {
+                name: "CSS",
+                bg: "bg-blue-600",
+            },
+            {
+                name: "JavaScript",
+                bg: "bg-amber-400",
+            },
+        ],
+        links: [
+            {
+                demo: {
+                    link: "https://numo-stanley.netlify.app",
+                    icon: <FaGlobe size={18} />,
+                },
+                github: {
+                    link: "https://github.com/Numostanley/MyPortfolio",
+                    icon: <FaCode size={18} />,
+                },
+            },
+        ],
+    },
+    {
         id: 6,
+        img: artGallery,
+        title: "Harvard Art Gallery",
+        description:
+            "A React frontend application that displays a list of art galleries. The information about the art galleries were extracted from the Harvard Art Museum API",
+        stack: [
+            {
+                name: "React",
+                bg: "bg-sky-500",
+            },
+            {
+                name: "CSS",
+                bg: "bg-blue-400",
+            },
+        ],
+        links: [
+            {
+                demo: {
+                    link: "https://youtu.be/E6z3twyKtDE",
+                    icon: <FaGlobe size={18} />,
+                },
+                github: {
+                    link: "https://github.com/Prechyious/long-practice-art-museum-project",
+                    icon: <FaCode size={18} />,
+                },
+            },
+        ],
+    },
+    {
+        id: 7,
         img: battleship,
         title: "Battleship",
         description:
